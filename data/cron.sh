20e3af9e2c7460c8aafa0a746d019973e1b8887c
Leonardo Gonzalez <Leonardo@EncryptedCommerce.net>
2020-03-17 17:44:48 -0500

    cron script for data updates

---
+++ data/cron.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+ wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv -O time_series_19-covid-Confirmed.csv
+ wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv -O time_series_19-covid-Deaths.csv
+ wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv -O time_series_19-covid-Recovered.csv
