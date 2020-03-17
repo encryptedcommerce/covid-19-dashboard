5121c69443c0dd55f6f842d4ed13b892f6b6ece5
Leonardo Gonzalez <Leonardo@EncryptedCommerce.net>
2020-03-17 22:48:08 +0000

    fixed shebang on cron script

--- data/cron.sh
+++ data/cron.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 pushd /home/ubuntu/covid-19/data # change to your installation dir
 wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv -O time_series_19-covid-Confirmed.csv
 wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv -O time_series_19-covid-Deaths.csv
