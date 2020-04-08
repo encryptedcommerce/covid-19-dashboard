616455442a3b0e938bf6336d394e5795d93443a0
Leonardo Gonzalez <Leonardo@EncryptedCommerce.net>
2020-04-08 15:23:17 +0000

    updated data source URLs

--- data/cron.sh
+++ data/cron.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pushd /home/ubuntu/covid-19/data # change to your installation dir
-wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv -O time_series_19-covid-Confirmed.csv
-wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv -O time_series_19-covid-Deaths.csv
-wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv -O time_series_19-covid-Recovered.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv -O time_series_19-covid-Confirmed.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv -O time_series_19-covid-Deaths.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv -O time_series_19-covid-Recovered.csv
 popd
